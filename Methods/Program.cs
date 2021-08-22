227050239d21527796d02f9c34b04c39cff799c5
Greg Zelner <gzelner1@gmail.com>
2021-08-22 12:02:40 -0400

    Add project files.

---
+++ Methods/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        static void AskAQuestion()
+        {
+            Console.WriteLine("Welcome to My Guessing Game!");
+            Console.WriteLine("Do you want to continue?");
+
+        }
+
+        static void User
+    }
+}
